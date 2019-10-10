b42928c0b01463cf828fb42ae790f9a1535e7002
pbillerot <philippe.billerot@gmail.com>
2019-10-10 12:21:33 +0200

    Bon pour test

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ fi
 
 VENV=venv
 
-if [ ! -d "$VENV" ]; thenls -l 
+if [ ! -d "$VENV" ]; then
     PYTHON=`which python3`
 
     if [ -f "$PYTHON" ]
@@ -17,6 +17,6 @@ if [ ! -d "$VENV" ]; thenls -l
     virtualenv -p $PYTHON $VENV
 fi
 
-. $VENV/bin/activate
+source $VENV/bin/activate
 
 pip3 install -r requirements.txt
